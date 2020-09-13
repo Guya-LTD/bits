db454b80110ad2f84a74fc51abc382a0030be00d
Simonbelete <simonbelete@gmail.com>
2020-09-13 08:30:16 +0300

    chore(save-me): save me commit

---
+++ src/components/templates/landing/Shop/Shop.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withNaming } from '@bem-react/classname'
+
+/**
+ * A Layout component represents an object or entity.
+ * 
+ * @author [Simon Belete](https://github.com/Simonbelete)
+ * @license [MIT](https://opensource.org/licenses/MIT)
+ * @project Guya E-commerce & Guya Express
+ * @see [Github](https://github.com/Guya-LTD/bits)
+ * @see [Bit.dev](https://bit.dev/guya-ltd/gcss)
+ * @description A Layout component represents an object or entity.
+ * @version Uses @bit [Semver v2.0.0](https://semver.org/spec/v2.0.0.html)
+ * @component Service
+ * @name Service
+ * @private
+ * @example 
+ */
