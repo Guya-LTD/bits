6a0fe426d9ed65221401a32b8a284e1d5d0881e1
Simonbelete <simonbelete@gmail.com>
2020-10-11 21:17:21 +0300

    feat(hero): hero molecules

---
+++ src/components/molecules/Hero/Hero.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withNaming } from '@bem-react/classname';
+import { classnames } from '@bem-react/classnames';
+
+/**
+ * A Layout component represents an object or entity.
+ * 
+ * @author [Simon Belete](https://github.com/Simonbelete)
+ * @license [MIT](https://opensource.org/licenses/MIT)
+ * @project Guya E-commerce & Guya Express
+ * @see [Github](https://github.com/Guya-LTD/bits)
+ * @see [Bit.dev](https://bit.dev/guya-ltd/gcss)
+ * @description A Layout component represents an object or entity.
+ * @version Uses @bit [Semver v2.0.0](https://semver.org/spec/v2.0.0.html)
+ * @component Login
+ * @name Login
+ * @private
+ * @example 
+ */
+
+class Hero extends Component {
+    render() {
+        /* Props */
+        const{bsPrefix, variant, left, right}=this.props;
+
+        /* Class name generator */
+        const cn = withNaming({ e: '__', m: '', v: '--' })
+
+        /* Set base classname */
+        let classname = cn(bsPrefix)
+
+        /* Classnames */
+        const classnametext = classnames(
+            classname({'': variant}),
+        );
+
+        return(
+            <div className={classnametext + ' row'}>
+                <div className='col-xs hero__left'>
+                    {left}
+                </div>
+                <div className='col-xs hero__right'>
+                    {right}
+                </div>
+            </div>
+        )
+    }
+}
+
+Hero.propTypes = {
+    /**
+     * @description Base class name.
+     * @property {string}
+     * @default 'button' 
+     */
+    bsPrefix: PropTypes.string,
+}
+
+Hero.defaultProps = {
+    bsPrefix: 'hero',
+}
+
+export default Hero;
